9ac184f00aa09b0872ee50aa88cd117043c925a0
AnuragNBansode <79781491+Anurag-Bansode@users.noreply.github.com>
2025-05-20 12:42:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify(message="Hello Anurag -Dockered now! at 12:19 ")
+    return jsonify(message="Hello Anurag -Dockered now! at 12:42 ")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=4908)
